4acea5c95ee57ecf73473e0941f1b9eaa4535630
yb150187@gmail.com <yb150187@gmail.com>
2019-11-06 09:46:10 +0200

    Created login_page.py and Added locators in locators.py

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        #assert "login1" in self.browser.current_url, "word \"login\" not in current url"
+        assert "login" in self.browser.current_url, "'login' not in current url"
+
+    def should_be_login_form(self):
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "login_form isn't presented"
+
+    def should_be_register_form(self):
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), "register_form isn't presented"
