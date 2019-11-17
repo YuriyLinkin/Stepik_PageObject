675a23ae2acfb5a18176b637652cb737b6b869c2
ITStudio Yurii Balitskyi <ybalitskyi@itstudiodev.com>
2019-11-17 11:49:35 +0200

    Refactoring product_page & test_product_page

--- pages/product_page.py
+++ pages/product_page.py
@@ -29,9 +29,6 @@ class ProductPage(BasePage):
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
 
-    def should_not_be_success_message_after_adding_product_to_basket(self):
-        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is shown after adding product to basket"
-
     def message_should_be_disappeared_after_adding_product_to_basket(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Message isn't dissapeared after adding product to basket"
 
