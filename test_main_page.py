93db48dd0dc40bd54a5ea16618a18f616306e547
ITStudio Yurii Balitskyi <ybalitskyi@itstudiodev.com>
2019-11-26 09:47:00 +0200

    13 step PObject - Final refactoring(adding mark.need_review and creating pytest.ini)

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
+from .pages.basket_page import BasketPage
 
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -25,9 +26,7 @@ class TestLoginFromMainPage():
         page.open()
         page.should_be_login_link()
 
-'''
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-
     page = MainPage(browser, link)
     page.open()
     page.go_to_basket_page()
@@ -36,27 +35,4 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.should_be_message()
 
 
-def test_should_be_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-
-    login_page = LoginPage(browser, link)
-    login_page.open()
-    login_page.should_be_login_url()
-
-
-def test_should_be_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-
-    login_page = LoginPage(browser, link)
-    login_page.open()
-    login_page.should_be_login_form()
-
-
-def test_should_be_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-
-    login_page = LoginPage(browser, link)
-    login_page.open()
-    login_page.should_be_register_form()
-'''
 
